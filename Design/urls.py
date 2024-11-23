e2c6942acdf643288dc077e2a76e6b51f3e63300
Наталья Крашенинникова <gr431_krnvi@lab-402-4.tomtit-tomsk.ru>
2024-11-23 11:22:53 +0700

    two commit

--- Design/urls.py
+++ Design/urls.py
@@ -20,10 +20,12 @@ from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Designweb/', include('Designweb.urls')),
     path('', RedirectView.as_view(url='/Designweb/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
